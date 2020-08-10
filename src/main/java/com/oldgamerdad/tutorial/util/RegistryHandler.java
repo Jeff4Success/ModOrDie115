5cb089ad8f0b623e03898b7601459c084fdb0006
Jeff4Success <jeff4success2@gmail.com>
2020-08-09 22:13:07 -0400

    Up to tutorial video number 6

--- src/main/java/com/oldgamerdad/tutorial/util/RegistryHandler.java
+++ src/main/java/com/oldgamerdad/tutorial/util/RegistryHandler.java
@@ -1,8 +1,13 @@
 package com.oldgamerdad.tutorial.util;
 
 import com.oldgamerdad.tutorial.Tutorial;
+import com.oldgamerdad.tutorial.blocks.BlockItemBase;
+import com.oldgamerdad.tutorial.blocks.RubyBlock;
 import com.oldgamerdad.tutorial.items.ItemBase;
+import com.oldgamerdad.tutorial.tools.ModItemTier;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,14 +16,26 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
 
     public static final DeferredRegister<Item> ITEMS =  new DeferredRegister<>(ForgeRegistries.ITEMS, Tutorial.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Tutorial.MOD_ID);
 
     public static void init() {
 
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
 
     }
 
     // For Items Only
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    // For Tools Only
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(Tutorial.TAB)));
+
+    // For Blocks Only
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+
+    // For Block Items Only
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
 
 }
